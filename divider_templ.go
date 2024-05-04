a4ceadc8ef7e427a334d099c98daaf1277834de6
henrrius <henrrybrgt@gmail.com>
2024-05-04 17:23:45 -0400

    fixing again missing classname on divider

--- divider_templ.go
+++ divider_templ.go
@@ -51,7 +51,7 @@ func Divider(dv DividerAttrs) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		var templ_7745c5c3_Var2 = []any{dv.GetClassName()}
+		var templ_7745c5c3_Var2 = []any{"divider", dv.GetClassName()}
 		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var2...)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
